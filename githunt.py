a13a62c352cbebe258377bd9914ab18bfeb4c6ae
Srinivas <nandusrinivas33.cse@gmail.com>
2020-01-12 18:49:06 +0530

    add date parameter to browse by date

--- githunt.py
+++ githunt.py
@@ -1,5 +1,9 @@
 """ Browse most stared repositories by date on Github """
 
+# Standard library imports
+from datetime import datetime, timedelta
+
+# Third party imports
 import click
 import requests
 
@@ -8,12 +12,34 @@ API_URL = "https://api.github.com/search/repositories"
 
 @click.command()
 @click.option("--language", default="", help="language filter (eg: python)")
-def search(language):
-    """ Returns repositories based on the language
+@click.option(
+    "--date",
+    default="",
+    help="date in the ISO8601 format which is YYYY-MM-DD (year-month-day)",
+)
+def search(language, date):
+    """ Returns repositories based on the language.
         repositories are sorted by stars
     """
-    filters = {"sort": "stars", "order": "desc", "q": f"language:{language}"}
-    repositories = requests.get(API_URL, params=filters).json()
+
+    if not date:
+        start_date = datetime.fromisoformat(
+            datetime.utcnow().date().isoformat()
+        )  # today's timestamp in YYYY-MM-DD:00:00:00 format
+        end_date = datetime.fromisoformat(
+            (datetime.utcnow() + timedelta(days=1)).date().isoformat()
+        )  # next day's timestamp in YYYY-MM-DD:00:00:00 format
+    else:
+        start_date = datetime.fromisoformat(date)
+        end_date = datetime.fromisoformat(
+            (start_date + timedelta(days=1)).date().isoformat()
+        )
+
+    query = (
+        f"language:{language}+created:{start_date.isoformat()}..{end_date.isoformat()}"
+    )
+    url = f"{API_URL}?q={query}&sort=stars&order=desc"
+    repositories = requests.get(url).json()
     print(repositories)
 
 
