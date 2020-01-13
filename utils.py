1e76e63b806a3b7efb580da80c576b5ce7313048
Srinivas <nandusrinivas33.cse@gmail.com>
2020-01-13 11:51:54 +0530

    add tabulate format

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """ Formats repositories in console """
 
 from colorama import init, Fore, Style
+from tabulate import tabulate
 
 
 def colored_output(repos):
@@ -27,7 +28,21 @@ def colored_output(repos):
         print(Fore.WHITE, Style.BRIGHT, seperator)
 
 
+def tabular_output(repos):
+    """ Displays repositories as tables using tabulate """
+    table_headers = ["Name", "URL", "Language", "Stars"]
+    repositories = [
+        [repo["full_name"], repo["html_url"], repo["stargazers_count"]]
+        for repo in repos
+    ]
+    print(tabulate(repositories, headers=table_headers, tablefmt="fancy_grid"))
+
+
 def beautify(repos, display_format):
     """ Beautfies the output based on display format given """
     if display_format == "colored":
         colored_output(repos)
+    elif display_format == "table":
+        tabular_output(repos)
+    else:
+        print("Can't output anything. Invalid display format!")
